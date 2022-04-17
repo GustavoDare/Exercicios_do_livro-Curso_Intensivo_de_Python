2ebb373b2585f1fee8f43666e85ef18d5e763346
GustavoDare <gustborgdare@gmail.com>
2022-04-17 14:56:59 -0300

    Modificação - máximo de 80 caracteres por linha

--- Capítulo 4/exercicio02.py
+++ Capítulo 4/exercicio02.py
@@ -1,8 +1,9 @@
 # Autor: Gustavo Daré
 # Data: 11/04/2022
 
-# Criar uma lista com 3 animais diferentes com uma característica em comum, escrever uma mensagem para cara animal
-#     (utilizando for) e escrever uma mensagem fora do laço for falando quando a característica em comum deles.
+# Criar uma lista com 3 animais diferentes com uma característica em comum, 
+#     escrever uma mensagem para cara animal (utilizando for) e escrever uma 
+#     mensagem fora do laço for falando quando a característica em comum deles.
 
 aves = ["gavião", "urubu", "pelicano"]
 for ave in aves:
