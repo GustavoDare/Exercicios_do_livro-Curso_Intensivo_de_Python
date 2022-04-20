6a2c4b165d7498545d405c7bec59188c710b644d
GustavoDare <gustborgdare@gmail.com>
2022-04-20 17:00:02 -0300

    Exercícios 1 e 2 do capítulo 5

---
+++ Capítulo 5/exercicio01.py
@@ -0,0 +1,32 @@
+# Autor: Gustavo Daré
+# Data: 20/04/2022
+
+# Escreva 10 testes condicionais e exiba qual o teste e o resultado
+#     previsto. Deve ter 5 resultados True e 5 resultados False.
+
+comida_preferida = "estrogonofe"
+esporte_preferido = "handebol"
+cor_preferida = "vermelho"
+idade = 21
+ano_nascimento = 2000
+
+print(f"Minha comida preferida é estrogonofe? Prevejo que é Verdade.")
+print(comida_preferida.lower() == "estrogonofe")
+print(f"Minha comida preferida é beringela? Prevejo que é Falso.")
+print(comida_preferida.lower() == "beringela")
+print(f"Meu esporte preferido é handebol? Prevejo que é Verdade.")
+print(esporte_preferido.lower() == "handebol")
+print(f"Meu esporte preferido é futebol? Prevejo que é Flaso.")
+print(esporte_preferido.lower() == "futebol")
+print(f"Minha cor preferida é vermelho? Prevejo que é Verdade.")
+print(cor_preferida.lower() == "vermelho")
+print(f"Minha cor preferida é amarelo? Prevejo que é Falso.")
+print(cor_preferida.lower() == "amarelo")
+print(f"Minha idade é 21? Prevejo que é Verdade.")
+print(idade == 21)
+print(f"Minha idade é 25? Prevejo que é Falso.")
+print(idade == 25)
+print(f"Meu ano de nascimento é 2000? Prevejo que é Verdade.")
+print(ano_nascimento == 2000)
+print(f"Meu ano de nascimento é 1995? Prevejo que é Falso.")
+print(ano_nascimento == 1995)
