95729a41fa935b60137a63874535a915fed63e89
GustavoDare <gustborgdare@gmail.com>
2022-06-08 11:22:16 -0300

    Exercícios 6,7,8 do Capítulo 8

---
+++ Capítulo 8/exercicio07.py
@@ -0,0 +1,33 @@
+# Autor: Gustavo Daré
+# Data: 08/06/2022
+
+'''
+Escreva uma função chamada make_album() que construa um dicionário 
+    descrevendo um álbum musical. A função deve aceitar o nome de um 
+    artista e o título de um álbum e deve devolver um dicionário 
+    contendo essas duas informações. Use a função para criar três 
+    dicionários que representem álbuns diferentes. Apresente cada valor 
+    devolvido para mostrar que os dicionários estão armazenando as 
+    informações do álbum corretamente.
+Acrescente um parâmetro opcional em make_album() que permita armazenar 
+    o número de faixas em um álbum. Se a linha que fizer a chamada 
+    incluir um valor para o número de faixas, acrescente esse valor ao 
+    dicionário do álbum. Faça pelo menos uma nova chamada da função 
+    incluindo o número de faixas em um álbum.
+'''
+
+def make_album(artista, titulo, faixas=''):
+    if faixas:
+        album = {'artista': artista.title(), 'titulo': titulo.title(),
+                    'n_faixas': faixas}
+    else:
+        album = {'artista': artista.title(), 'titulo': titulo.title()}
+    return album
+
+album1 = make_album('michael jackson', 'thriller')
+album2 = make_album('taylor swift', '1989', 13)
+album3 = make_album('dua lipa', 'future nostalgia', 11)
+
+print(album1)
+print(album2)
+print(album3)
---
+++ Capítulo 8/exercicio08.py
@@ -0,0 +1,27 @@
+# Autor: Gustavo Daré
+# Data: 08/06/2022
+
+'''
+Comece com o seu programa do Exercício 8.7. Escreva um laço while que 
+    permita aos usuários fornecer o nome de um artista e o título de um 
+    álbum. Depois que tiver essas informações, chame make_album() com 
+    as entradas do usuário e apresente o dicionário criado. Lembre-se 
+    de incluir um valor de saída no laço while.
+'''
+
+def make_album(artista, titulo):
+    album = {'artista': artista.title(), 'titulo': titulo.title()}
+    return album
+
+albuns = []
+print("REGISTRO DE ÁLBUNS\n")
+while True:
+    print("Caso não queira registrar mais nenhum álbum digite: 'OK'")
+    artista = input("Informe o nome do artista: ")
+    if artista.upper() == 'OK':
+        break
+    titulo = input("Informe o título do álbum: ")
+    albuns.append(make_album(artista, titulo))
+
+for album in albuns:
+    print(album)
